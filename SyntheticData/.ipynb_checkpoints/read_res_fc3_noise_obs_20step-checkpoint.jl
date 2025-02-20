b5774db2a629148ab909a0e6a26d1e08299fec56
tjyukihira <tyfisheries@gmail.com>
2025-02-20 22:57:33 +0900

    remove old files

--- SyntheticData/.ipynb_checkpoints/read_res_fc3_noise_obs_20step-checkpoint.jl
+++
@@ -1,4 +0,0 @@
-vec_res_fc3 = Vector{Tuple}(undef, 30)
-for i in 1:30
-    vec_res_fc3[i] = load_gpresults_jld2("./results/fc3_noise_obs_20step_dist/0809_fc3_noise_obs_20step_model$(i)_se.jld2";npop=3, jacobian=true)
-end
